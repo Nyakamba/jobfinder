7a31a405ea4f20c549b04bbe81db9603d31baaa2
Nyakamba <omwegaenock@gmail.com>
2024-03-19 22:01:43 +0300

    assets

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
+import { Outlet, Navigate, Route, Routes, useLocation } from "react-router-dom";
+import { Footer, Navbar } from "./components";
+
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Navbar />
+
+      <Footer />
     </div>
   );
 }
