b707d68105cc31b0abc3c7f05b5fc2dbeedf98a7
Nyakamba <omwegaenock@gmail.com>
2024-03-20 22:05:20 +0300

    find job page

--- client/src/pages/FindJobs.jsx
+++ client/src/pages/FindJobs.jsx
@@ -1,5 +1,37 @@
+import { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { Header } from "../components";
+
 const FindJobs = () => {
-  return <div>FindJobs</div>;
+  const [sort, setSort] = useState("Newest");
+  const [page, setPage] = useState(1);
+  const [numPage, setNumPage] = useState(1);
+  const [recordCount, setRecordCount] = useState(0);
+  const [data, setData] = useState([]);
+
+  const [searchQuery, setSearchQuery] = useState("");
+  const [jobLocation, setJobLocation] = useState("");
+  const [filterJobTypes, setFilterJobTypes] = useState([]);
+  const [filterExp, setFilterExp] = useState([]);
+
+  const [isFetching, setIsFetching] = useState(false);
+
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <Header
+        title="Find Your Dream Job With Ease"
+        type="home"
+        handleClick={() => {}}
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+        location={jobLocation}
+        setLocation={setJobLocation}
+      />
+    </div>
+  );
 };
 
 export default FindJobs;
