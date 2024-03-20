ace978fdd21e788ee2900443217a69f60e852e78
Nyakamba <omwegaenock@gmail.com>
2024-03-20 22:05:31 +0300

    header component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,6 +1,7 @@
 import CustomButton from "./CustomButton";
 import Footer from "./Footer";
+import Header from "./Header";
 import Navbar from "./Navbar";
 import SignUp from "./SignUp";
 
-export { Footer, Navbar, CustomButton, SignUp };
+export { Footer, Navbar, CustomButton, SignUp, Header };
