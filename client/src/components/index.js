af00b564fea4558abcc862cdeb06b7ec8f2ee33f
Nyakamba <omwegaenock@gmail.com>
2024-03-20 22:56:13 +0300

    jobcard

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,5 +3,7 @@ import Footer from "./Footer";
 import Header from "./Header";
 import Navbar from "./Navbar";
 import SignUp from "./SignUp";
+import JobCard from "./JobCard";
+import ListBox from "./ListBox";
 
-export { Footer, Navbar, CustomButton, SignUp, Header };
+export { Footer, Navbar, CustomButton, SignUp, Header, JobCard, ListBox };
