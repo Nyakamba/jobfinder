a2f1bcc31ca58af12630cbe8e41a3b04558f3140
Nyakamba <omwegaenock@gmail.com>
2024-03-20 11:11:28 +0300

    complete navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -34,6 +34,60 @@ function MenuList({ user, onClick }) {
             />
           </Menu.Button>
         </div>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute z-50 right-2 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg focus:outline-none">
+            <div className="p-1">
+              <Menu.Item>
+                {({ active }) => (
+                  <Link
+                    to={`${
+                      user?.accountType ? "user-profile" : "company-profile"
+                    }`}
+                    className={`${
+                      active ? "bg-blue-500 text-white" : "text-gray-900"
+                    } group flex w-full items-center rounded-md p-2 text-sm`}
+                    onClick={onClick}
+                  >
+                    <CgProfile
+                      className={`${
+                        active ? "text-white" : "text-gray-600"
+                      } mr-2 h-5 w-5  `}
+                      aria-hidden="true"
+                    />{" "}
+                    {user?.accountType ? "User Profile" : "Company Profile"}
+                  </Link>
+                )}
+              </Menu.Item>
+
+              <Menu.Item>
+                {({ active }) => (
+                  <button
+                    onClick={() => handleLogout()}
+                    className={`${
+                      active ? "bg-blue-500 text-white" : "text-gray-900"
+                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                  >
+                    <AiOutlineLogout
+                      className={`${
+                        active ? "text-white" : "text-gray-600"
+                      } mr-2 h-5 w-5  `}
+                      aria-hidden="true"
+                    />
+                    Log Out
+                  </button>
+                )}
+              </Menu.Item>
+            </div>
+          </Menu.Items>
+        </Transition>
       </Menu>
     </div>
   );
@@ -56,7 +110,6 @@ const Navbar = () => {
               Job<span className="text-[#1677cccb]">Finder</span>
             </Link>
           </div>
-
           <ul className="hidden lg:flex gap-10 text-base">
             <li>
               <Link to="/find-jobs">Find Job</Link>
@@ -85,7 +138,52 @@ const Navbar = () => {
               </div>
             )}
           </div>
+          <button
+            className="block lg:hidden text-slate-900"
+            onClick={() => setIsOpen((prev) => !prev)}
+          >
+            {isOpen ? <AiOutlineClose size={26} /> : <HiMenuAlt3 size={26} />}
+          </button>
         </nav>
+        {/* mobile menu */}
+        <div
+          className={`${
+            isOpen ? "absolute flex bg-[#f7fdfd] " : "hidden"
+          } container mx-auto lg:hidden flex-col pl-8 gap-3 py-5`}
+        >
+          <Link to="/" onClick={handleCloseNavbar}>
+            Find Job
+          </Link>
+          <Link to="/companies" onClick={handleCloseNavbar}>
+            Companies
+          </Link>
+          <Link
+            onClick={handleCloseNavbar}
+            to={
+              user?.accountType === "seeker" ? "applly-gistory" : "upload-job"
+            }
+          >
+            {user?.accountType === "seeker" ? "Applications" : "Upload Job"}
+          </Link>
+          <Link to="/about-us" onClick={handleCloseNavbar}>
+            About
+          </Link>
+
+          <div className="w-full py-10">
+            {!user?.token ? (
+              <a href="/user-auth">
+                <CustomButton
+                  title="Sign In"
+                  containerStyles={`text-blue-600 py-1.5 px-5 focus:outline-none hover:bg-blue-700 hover:text-white rounded-full text-base border border-blue-600`}
+                />
+              </a>
+            ) : (
+              <div>
+                <MenuList user={user} onClick={handleCloseNavbar} />
+              </div>
+            )}
+          </div>
+        </div>
       </div>
     </>
   );
