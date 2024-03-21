29167f14f45fc9cfb8f584953a253d13e39610f4
Nyakamba <omwegaenock@gmail.com>
2024-03-21 13:54:29 +0300

    mongodb connection

---
+++ server/server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+
+import xss from "xss-clean";
+import mongoSanitize from "mongo-sanitize";
+import dbConnection from "./dbConfig/dbConnection.js";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 8800;
+
+//Mongodb connection
+dbConnection();
+
+//middlewares
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(morgan("dev"));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
