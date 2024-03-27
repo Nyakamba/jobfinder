07d59b3e461c749b5fe77fc52ee17221c2ea9116
Nyakamba <omwegaenock@gmail.com>
2024-03-27 21:13:55 +0300

    job card

--- client/src/components/JobCard.jsx
+++ client/src/components/JobCard.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 
 const JobCard = ({ job }) => {
   //const { user } = useSelector((state) => state.user);
+  //const { user } = useSelector((state) => state.user);
   return (
     <Link to={`/job-detail/${job?._id}`}>
       <div
