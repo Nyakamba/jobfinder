d4ccd390edb5acc3d485733f5d9a01441ee3d768
Nyakamba <omwegaenock@gmail.com>
2024-05-07 17:42:24 +0300

    profile

--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -65,7 +65,7 @@ const UserForm = ({ open, setOpen }) => {
                     className="w-full mt-2 flex flex-col gap-5"
                     onSubmit={handleSubmit(onSubmit)}
                   >
-                    <di className="w-full flex gap-2">
+                    <div className="w-full flex gap-2">
                       <div className="w-1/2">
                         <TextInput
                           name="firstName"
@@ -94,7 +94,7 @@ const UserForm = ({ open, setOpen }) => {
                           }
                         />
                       </div>
-                    {/* </diff */}
+                    </div>
                     <div className="w-full flex gap-2">
                       <div className="w-1/2">
                         <TextInput
